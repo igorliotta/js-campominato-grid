88e05bb16ab68ac913239ff7b0fd470b80bf347b
igorliotta <liottaigor@gmail.com>
2023-10-16 15:08:53 +0200

    Fix:

---
+++ js/bonus.js
@@ -0,0 +1,37 @@
+// console.log('BONUS')
+
+// const selectDOMElement = document.getElementById('select');
+// console.log(selectDOMElement);
+
+// selectDOMElement.addEventListener('click', function () {
+//     console.log('funziona');
+// })
+
+
+// const optionDOMElements = document.querySelectorAll('option');
+// console.log(optionDOMElements);
+
+
+// for (let i = 0; i < optionDOMElements.length; i++) {
+
+//     const currentOption = [i + 1];
+//     console.log('Hai selezionato la difficoltà ' + currentOption);
+// }
+
+
+// currentOption.addEventListener('click', function () {
+//     console.log('funziona')
+// })
+
+// const optionDOMElements = document.querySelectorAll('.option');
+//    console.log(optionDOMElements);
+
+// for (let i = 0; i < optionDOMElements.length; i++) {
+
+//     const currentOption = optionDOMElements[i];
+//     console.log(currentOption);
+
+//     currentOption.addEventListener('click', function () {
+//         console.log('funziona');
+//     })
+// }
